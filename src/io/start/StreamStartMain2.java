376deee764c820ff18dd838bb1827fc05cef9d31
rojojun <zoloman316@gmail.com>
2025-06-27 17:57:10 +0900

    파일 스트림 예시 #2

---
+++ src/io/start/StreamStartMain2.java
@@ -0,0 +1,22 @@
+package io.start;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class StreamStartMain2 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("temp/hello.dat");
+        byte[] input = {65, 66, 67, 68};
+        fileOutputStream.write(input);
+        fileOutputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("temp/hello.dat");
+        byte[] buffer = new byte[10];
+        int readCount = fileInputStream.read(buffer, 0, 10);
+        System.out.println("readCount = " + readCount);
+        System.out.println(Arrays.toString(buffer));
+        fileInputStream.close();
+    }
+}
