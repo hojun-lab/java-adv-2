aaafc38086746114ca39996759a2f22e8c925a82
rojojun <zoloman316@gmail.com>
2025-07-06 22:08:24 +0900

    자원 예외 처리 마무리

---
+++ src/network/tcp/v6/SessionV6.java
@@ -0,0 +1,68 @@
+package network.tcp.v6;
+
+import network.tcp.SocketCloseUtil;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class SessionV6 implements Runnable {
+    private final Socket socket;
+    private final DataInputStream input;
+    private final DataOutputStream output;
+    private final SessionManagerV6 sessionManager;
+    private boolean closed = false;
+
+    public SessionV6(Socket socket, SessionManagerV6 sessionManager) throws IOException {
+        this.socket = socket;
+        this.input = new DataInputStream(socket.getInputStream());
+        this.output = new DataOutputStream(socket.getOutputStream());
+        this.sessionManager = sessionManager;
+        this.sessionManager.add(this);
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            while (true) {
+                // 클라이언트로부터 문자 받기
+                String received = input.readUTF();
+                log("client -> server: " + received);
+
+                if (received.equals("exit")) {
+                    break;
+                }
+
+                // 클라이언트로부터 문자 보내기
+                String toSend = received + " World!";
+                output.writeUTF(toSend);
+                log("client <- server: " + toSend);
+            }
+
+            // 자원 정리
+            log("연결 종료: " + socket);
+            input.close();
+            output.close();
+            socket.close();
+        } catch (IOException e) {
+            log(e);
+        } finally {
+            sessionManager.remove(this);
+            close();
+        }
+    }
+
+    public void close() {
+        if (closed) {
+            return;
+        }
+
+        SocketCloseUtil.closeAll(socket, input, output);
+        closed = true;
+        log("연결 종료: " + socket + " isClosed: " + socket.isClosed());
+    }
+}
