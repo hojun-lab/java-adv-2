151e0e4725128688a11d29bcff7867a235621034
rojojun <zoloman316@gmail.com>
2025-07-10 21:25:38 +0900

    채팅 CLI Server 생성 #3

--- src/chatting_v1/Server.java
+++ src/chatting_v1/Server.java
@@ -1,4 +1,24 @@
 package chatting_v1;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
 public class Server {
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("서버 시작");
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        log("서버 소켓 시작 - 리스닝 포트 : " + PORT);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            Session session = new Session(socket);
+            new Thread(session).start();
+        }
+    }
 }
