da9818a3698f1263cfb5fef54b2127de89898e4e
rojojun <zoloman316@gmail.com>
2025-07-16 20:10:16 +0900

    리플랙션 학습

---
+++ src/reflection/data/BasicData.java
@@ -0,0 +1,29 @@
+package reflection.data;
+
+public class BasicData {
+    public String publicField;
+    private int privateField;
+
+    public BasicData() {
+        System.out.println("BasicData.BasicData");
+    }
+
+    private BasicData(String data) {
+        System.out.println("BasicData.BasicData: " + data);
+    }
+
+    public void call() {
+        System.out.println("BasicData.call");
+    }
+
+    public String hello(String str) {
+        System.out.println("BasicData.hello");
+        return str + " hello";
+    }
+
+    void defaultMethod() {
+        System.out.println("BasicData.defaultMethod");
+    }
+
+
+}
